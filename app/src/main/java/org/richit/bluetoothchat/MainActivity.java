73396dcd9100219d0a79c9018fb43d636a6e2a38
Md Rayhan <mdrayhan9464@gmail.com>
2020-02-24 10:43:11 +0600

    permission added

--- app/src/main/java/org/richit/bluetoothchat/MainActivity.java
+++ app/src/main/java/org/richit/bluetoothchat/MainActivity.java
@@ -2,13 +2,26 @@ package org.richit.bluetoothchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+
+
+
     }
+
+
+
 }
